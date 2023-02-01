01e63722878418aa9d38c3725ae9fd49f75cd0af
takeshiho0531 <kawadatakeshiho@gmail.com>
2023-02-02 05:51:35 +0900

    feat: 非線形方程式の解法の1つである関数反復法の実装をしてみた

---
+++ nonlinear_equation/functional_iteration.py
@@ -0,0 +1,16 @@
+# 非線形方程式の解法
+# Contents
+# 関数反復法(浮動点反復法)
+def g(x):
+    #f(x)=0から同値変形でx=g(x)にする
+    # return
+    pass
+
+def functional_iterarion(x, tol): #tol: 許容誤差
+    x=0
+    delta=0
+    while delta>tol:
+        y=g(x)
+        delta=y-x
+        x=y
+    return x
